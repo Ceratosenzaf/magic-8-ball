6d7dcc6e7ce610a0f08e02e56723a711650d2b95
Davide Cerato <mrc3drik@gmail.com>
2020-06-05 20:42:21 +0200

    8balls

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,50 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.lightBlue,
+          appBar: AppBar(
+            backgroundColor: Colors.lightBlue[800],
+            title: Center(
+              child: Text(
+                "Ask Me Anthing",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+          ),
+          body: BallPage(),
+        ),
       ),
     );
+
+class BallPage extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  int ballNum = 1;
+
+  void pickNum() {
+    setState(() {
+      ballNum = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          pickNum();
+        },
+        child: Image.asset('images/ball$ballNum.png'),
+      ),
+    );
+  }
+}
